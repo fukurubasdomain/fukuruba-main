b9927c92201adfec8eee6d10ae75aaf5367a7024
fukurubasdomain <abougrine022@gmail.com>
2026-09-24 19:10:01 +0100

    Add watchly-settings.js with idle messages

---
+++ watchly-settings.js
@@ -0,0 +1,13 @@
+//Taking a little break.
+//Not doing much right now.
+//sleeping😴.
+//Probably touching grass.
+//Somewhere between sleepy and bored.
+//On my way home from uni.
+//At the gym, working on myself.
+//At the library, fighting for my life.
+//Studying for an exam I forgot about.
+//In class.
+window.watchlySettings = {
+  idleMessage: "Probably touching grass."
+};
